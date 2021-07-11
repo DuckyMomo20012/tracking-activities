145559d4972cc77be887994d5f846999d9fc48e9
Qhope <19127410@student.hcmus.edu.vn>
2021-07-11 14:06:15 +0700

    huong dan

--- Test2/MyForm.h
+++ Test2/MyForm.h
@@ -21,10 +21,10 @@ namespace Test2 {
 	using namespace std;
 	using namespace System::Threading;
 
-static bool login1;
+static bool loginFlag;
 static void wait15Second() {
 		Sleep(15000);
-		if (login1==false)
+		if (loginFlag ==false)
 		{
 			MessageBox::Show("Done");
 		}
@@ -39,19 +39,12 @@ static void wait15Second() {
 		MyForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			// 
-			//
-
 		}
 	public:
 		int wrongPass = 0;
-		//bool login1;
+	
 	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
+
 		~MyForm()
 		{
 			if (components)
@@ -69,19 +62,14 @@ static void wait15Second() {
 	protected:
 
 	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
+		
 		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
+		
 		void InitializeComponent(void)
 		{
-
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
@@ -89,41 +77,33 @@ static void wait15Second() {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(40, 88);
+			resources->ApplyResources(this->textBox1, L"textBox1");
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(131, 22);
-			this->textBox1->TabIndex = 0;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(198, 86);
+			resources->ApplyResources(this->button1, L"button1");
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 1;
-			this->button1->Text = L"Login";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// label1
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point( 109, 160);
+			resources->ApplyResources(this->label1, L"label1");
+			this->label1->ForeColor = System::Drawing::Color::Black;
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(46, 17);
-			this->label1->TabIndex = 2;
-			this->label1->Text = L"label1";
+			this->label1->UseCompatibleTextRendering = true;
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			resources->ApplyResources(this, L"$this");
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(282, 253);
+			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->ControlBox = false;
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox1);
-			this->ControlBox = false;
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
@@ -140,63 +120,107 @@ static void wait15Second() {
 
 
 		if (convert.marshal_as<string>(textBox1->Text) == ParentPass) {
-			//this->Hide();
-			//May be work
-			/*chrono::seconds oneHour(30);
+			this->Hide();
+			///May be work
+			chrono::hours oneHour(1);
 			this_thread::sleep_for(oneHour);
-			this->Show();*/
-			mainPage^ m1 = gcnew mainPage();
-			login1 = true;
-			m1->ShowDialog();
+			this->Show();
+			
+			loginFlag = true;
+
 
 
 		}
-		else {
-			login1 = false;
+		else if(convert.marshal_as<string>(textBox1->Text) != ParentPass){
+			
+			loginFlag = false;
 			wrongPass++;
+
+
 			int getHour = 23;
 			int getMinute = 30;
 			time_t timer = time(0);
 			tm* now = localtime(&timer);
 			Time currentTime = Time(now->tm_hour, now->tm_min);
-			if (th1 == nullptr)
-			{
-				th1 = gcnew Thread(gcnew ThreadStart(wait15Second));
-				th1->Start();
-				
-			}
 
+			//Doc file txt xem co o trong thoi gian su dung hay khong
+			//Trong 1 phien hd chi can doc 1 lan
 
-
-			if (currentTime._hour < getHour)
+			bool accept=true;
+			// gia su ko cho phep dung
+			//Neu khong duoc su dung
+			if (!accept)
 			{
 
 				// New thread zone and shutdown
+				if (th1 == nullptr)
+				{
+					th1 = gcnew Thread(gcnew ThreadStart(wait15Second));
+					th1->Start();
 
+				}
 
 				//Cont
 				
 
 
 			}
-			else if (currentTime._hour >= getHour) {
+			//gia su cho dung
+			//Nau duoc su dung
+			else if (accept==true)
+			{
 				if (convert.marshal_as<string>(textBox1->Text) == childPass)
 				{
-					MessageBox::Show("Con 10 p nhua la het gio", "Time Left");
-					Sleep(10);
-					this->Hide();
-					mainPage^ main = gcnew mainPage;
-					main->ShowDialog();
+					this->textBox1->Hide();
+					this->button1->Hide();
+					this->label1->Text = L"Ban da dang nhap thanh cong";
+					this->label1->Show();
+					//Kiem tra con bao lau thi het gio
+					String^ msg;
+					MessageBox::Show(msg, "Time Left"); //thong bao
 
-					Sleep(10);
 
-					this->Close();
+					//Thong bao xong thi dong Login page
+					Sleep(10);
+					//this->Hide(); //Chay ngam
+
+
+					//while(moi 5 phut){
+					// 
+					// Sleep(5phut)
+					// Goi ham lay file txt tu Drive, vi wifi co the lag nen moi nguoi cho Sleep() mot luc, tuy tinh hinh
+					// Sleep()
+					// 
+					// 
+					//chup man hinh va ghi vo thu muc Upload, chua co thi tao
+					// Upload len, nho upload xong phai tu tat script vua chay
+					// 
+					// 
+					//Doc file txt xem co thay doi hay khong, co thi cap nhat
+					//if(co thay doi){
+					// cap nhat;
+					// Thong bao; 
+					//}
+					//
+
+					//}
+
+					//this->Close();
 
 				}
 				else {
 					if (wrongPass == 3)
 					{
-						this->Close();
+						//C2.1.2.1
+						//Neu sai 3 lan se chay vo day
+						//Lay thoi gian hien tai cong them 10p roi ghi vo txt 
+						
+
+						this->Close(); //Close roi moi shutdown
+						//Shutdown
+						
+
+
 						//
 						// Time ten;
 						// Plus 10 to ten
@@ -211,11 +235,12 @@ static void wait15Second() {
 
 	}
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
-
+		this->label1->Hide();
 	}
 
 		  
-	};
+	
+};
 
 
 }
